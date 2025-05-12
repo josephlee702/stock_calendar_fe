9d901043d875b16a601f8f230b89451267d02cbe
Joseph Lee <jhjlee702@gmail.com>
2025-05-12 16:41:57 -0600

    port over homepage from stock portfolio app

---
+++ src/pages/HomePage.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState, useContext } from "react";
+import api from "../services/api"; 
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/Authcontext";
+
+const HomePage = ({ darkMode }) => {
+  const {user} = useContext(AuthContext);
+  // const [portfolios, setPortfolios] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchPortfolios = async () => {  
+  //     try {
+  //       const response = await api.get("/portfolios", {
+  //         headers: {
+  //           "access-token": localStorage.getItem("access-token"),
+  //           "client": localStorage.getItem("client"),
+  //           "uid": localStorage.getItem("uid"),
+  //           "Content-Type": "application/json",
+  //           "Accept": "application/json",
+  //         },
+  //         withCredentials: true,
+  //       });
+  //       setPortfolios(response.data);
+  //       console.log(response.data);
+  //     } catch (error) {
+  //       console.error("Error fetching portfolios:", error);
+  //     }
+  //   };
+  //   fetchPortfolios();
+  // }, [user]);
+  
+
+  return (
+    <div className="container mt-4">
+      <h1 className={`display-4 ${darkMode ? "text-light" : "text-dark"}`}> Your Trade Calendar</h1>
+      {/* {portfolios.length > 0 ? (
+        <ul className="list-group">
+          {portfolios.map((portfolio) => (
+            <li key={portfolio.id} className="list-group-item">
+              <h2><Link to={`/portfolios/${portfolio.id}`}>{portfolio.name}</Link></h2>
+              <p>Total Market Value: ${parseFloat(portfolio.total_market_value).toLocaleString("en-US", {minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No portfolios found.</p>
+      )} */}
+    </div>
+  );
+};
+
+export default HomePage;
